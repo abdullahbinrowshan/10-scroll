1584fce77ddd393f01b2141904b77e23407affd9
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-03-26 19:54:27 +0600

    fixed nav and scroll top btn added

--- app.js
+++ app.js
@@ -4,10 +4,46 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear()
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    // linksContainer.classList.toggle('show-links')
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    console.log(containerHeight); 
+    const linksHeight = links.getBoundingClientRect().height;
+    if (containerHeight == 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = '0'
+    }
+})
+
 
 // ********** fixed navbar ************
+
+const navbar = document.getElementById('nav');
+const topLinks = document.querySelector('.top-link');
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight >= navHeight) {
+        navbar.classList.add('fixed-nav')
+    }else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if (scrollHeight >= 500) {
+        topLinks.classList.add('show-link')
+    }else {
+        topLinks.classList.remove('show-link')
+    }
+})
 
 // ********** smooth scroll ************
 // select links
